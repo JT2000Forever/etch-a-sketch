73ca0ec4492e0bb5ef3db9454346513b5597aea1
Jonathan Turja <jt2000forever@gmail.com>
2017-01-26 20:28:10 +0100

    Gjorde färdigt sidan

--- javascript.js
+++ javascript.js
@@ -1,41 +1,33 @@
-var colN = 40;
-var rowN = 40;
-var width = 600/colN;
-var height = 600/rowN;
-/*
-$(document).ready(function() {
-	//Skapa rutnät
-	for (var x = 0; x < rowN; x++) {
-		for (var y = 0; y < colN; y++) {
-			var $ruta = $("<div class='ruta'></div>");
-			$ruta.height(height + "px");
-			$ruta.width(width + "px");
-			$('#wrapper').append($ruta);
-		}
-	}
-	
-	//Måla ruta
-	$(document).on('mouseover', '.ruta', function() {
-		$(this).css("background-color", "#DBDBDB");
-	});
-});
-*/
+var gridSize = 40;
+var dim = 600/gridSize;
 
 $(document).ready(function() {
 	//Skapa rutnät
-	for (var x = 0; x < rowN; x++) {
-		var $tr = $("<tr id='" + x +"'>");
-		$('table').append($tr);
-		for (var y = 0; y < colN; y++) {
-			var $td = $("<td>");
-			$td.height(height + "px");
-			$td.width(width + "px");
-			$tr.append($td);	
-		}
-	}
+	MakeGrid();
+	
+	//Reset
+	$('button').click(function() {
+		$('table').empty();
+		gridSize = prompt("Dimensioner? (t.ex 40)");
+		dim = 600/gridSize;
+		MakeGrid();
+	});
 	
 	//Måla ruta
 	$(document).on('mouseover', 'td', function() {
 		$(this).css("background-color", "#DBDBDB");
 	});
 });
+
+var MakeGrid = function() {
+	for (var x = 0; x < gridSize; x++) {
+		var $tr = $("<tr id='" + x +"'>");
+		$('table').append($tr);
+		for (var y = 0; y < gridSize; y++) {
+			var $td = $("<td>");
+			$td.height(dim + "px");
+			$td.width(dim + "px");
+			$tr.append($td);	
+		}
+	}
+}
